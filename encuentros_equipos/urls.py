940169a8d7bfe044025f030362ce3cfa190a7c13
Ander19 <ndrsnvengas@gmail.comgit config --global user.email>
2023-10-01 02:41:54 -0500

    creacion de jugador y update css del main panel

--- encuentros_equipos/urls.py
+++ encuentros_equipos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from encuentros_equipos.views import mis_resultados
+from encuentros_equipos.views import mis_resultados,añadir_jugador
 
 
 
 urlpatterns = [ 
-    path('panel/resultados/<int:user_id>',mis_resultados, name="resultado")
+    path('panel/resultados/<int:user_id>',mis_resultados, name="resultado"),
+    path("panel/añadir_jugador/<int:user_id>",añadir_jugador , name="añadir_jugador")
 ]
  
--- encuentros_equipos/views.py
+++ encuentros_equipos/views.py
@@ -6,9 +6,10 @@ from django.contrib.auth.decorators import login_required
 from equipos.models import *
 from campeonato.models import*
 from django.db.models import Q
+from django.urls import reverse
+import datetime
 
-
-@login_required(login_url='home')
+@login_required(login_url='login')
 def mis_resultados(request,user_id): 
 
     try:
@@ -35,3 +36,46 @@ def mis_resultados(request,user_id):
                                                    "resultados":resultado
                                                    })
 
+
+
+
+@login_required(login_url='login')
+def añadir_jugador(request,user_id):
+
+    try:
+        equipo=Equipo.objects.get(delegado=user_id)
+        usuario=equipo.delegado.id
+        url=reverse('panel_user',args=[usuario])
+        print(usuario)
+        print(equipo)
+        if request.method=="POST":
+            nombre=request.POST["nombre"]
+            apellidos=request.POST["apellidos"]
+            fecha_nacimiento=request.POST.get("fecha_nacimiento",datetime.datetime.now().date())
+            imagen_dni=request.FILES.get("imagen_dni")
+
+            print(nombre)
+            print(apellidos)
+            print(fecha_nacimiento)
+            print(imagen_dni)
+
+            nuevo_jugador=Jugadores(
+                Nombre=nombre,
+                apellidos=apellidos,
+                imagen_dni=imagen_dni,
+                fecha_nacimiento=fecha_nacimiento,
+                equipo=equipo
+            )
+
+            nuevo_jugador.save()
+
+            return redirect(url)
+
+
+    except Exception as e:
+        print(e)
+        return redirect('home')
+
+
+
+    return render(request,'panel_añadir_jugador.html',{"equipo":equipo})
